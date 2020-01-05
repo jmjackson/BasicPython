7341e2174dd9e0b5573cd2a47fa94a5122596021
jmjackson <jarceluz@gmail.com>
2020-01-05 14:01:03 -0800

    tipos de datos
    Conociendo los tipos de datos en python
    Investiigar algunos mas y probarlos

---
+++ basic.py
@@ -0,0 +1,15 @@
+# Este es una impresion de un string
+print("Hola Buenos dias")
+
+#y esta es una variable, Aqui las variables no tienen
+# un tipo de datos sino que python Acomoda el tipo de dato
+entero=5
+print("Imprimiendo un entero ",entero)
+
+double=2.5
+print("Imprimiendo un double ",double)
+
+cadena="Esta es una cadena de caracteres"
+print(Cadena)
+
+#En python no usamos el punto y coma (;)
