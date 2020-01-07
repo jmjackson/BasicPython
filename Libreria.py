13246e13911d7e2e16bd0794b8acaf1597f69de5
jmjackson <jarceluz@gmail.com>
2020-01-07 12:48:39 -0800

    Librerias
    Librerias

---
+++ Libreria.py
@@ -0,0 +1,7 @@
+## LAs librerias en python se usan importandolas
+##regularmente usamos las librerias especificas  de la sigiente manera
+from math import pi # Aqui solo importamos pi
+from math import * # con el asterisco importamos todo
+
+print(pi)
+print(sin(3))
