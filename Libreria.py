97b816fbc1065cc5579ae4f39ed5341ce41f055b
jmjackson <jarceluz@gmail.com>
2020-01-07 14:01:24 -0800

    mas de lib
    un poco mas

--- Libreria.py
+++ Libreria.py
@@ -2,6 +2,8 @@
 ##regularmente usamos las librerias especificas  de la sigiente manera
 from math import pi # Aqui solo importamos pi
 from math import * # con el asterisco importamos todo
-## INvestiga cuantas librerias puedes usar 
-print(pi)
-print(sin(3))
+## INvestiga cuantas librerias puedes usar
+print(pi) # numero pi
+print(sin(3)) #seno de 3
+print(sqrt(3)) # raiz cuadrada de 3
+print(pow(3,2)) #e elevada a la potencia de 2 (o como dicen en mi pueblo 3 elevada al cuadrado)
